fbee9dd5cb4a51029bd72dc72ee129bde8609142
Vineww <133378253+Vineww@users.noreply.github.com>
2023-10-23 14:33:15 -0300

    V1.0.3

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,39 @@ using namespace std;
 int main(){
 
     BinaryTree arvore;
-    ifstream LerArquivo("exemplo1.tree");
     string s;
+    int num;
+    
+    cout << "Selecione uma das opcoes: \n";
+    cout << "1.ler exemplo1 \n";
+    cout << "2.ler exemplo2 \n";
+    cin >> num;
+
+    while (num >=3){
+            cout << "Selecione uma das opcoes: \n";
+            cout << "1.ler exemplo1 \n";
+            cout << "2.ler exemplo2 \n";
+            cin >> num;
+
+    }
+    
+    if(num == 1){
+    ifstream LerArquivo("exemplo1.tree");
     while(getline(LerArquivo, s))
             arvore.insert(s);
     arvore.PreOrdem();
     LerArquivo.close();
+    }
+
+    if(num == 2){
+    ifstream LerArquivo("exemplo2.tree");
+    while(getline(LerArquivo, s))
+            arvore.insert(s);
+    arvore.PreOrdem();
+    LerArquivo.close();
+    }
+
+
     return 0;
 
 }
